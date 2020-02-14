6af66ece1c7a6069b5de899e9a6897dfc5182a84
rouxcaesar <jrzerwick@gmail.com>
2020-02-14 09:29:15 -0500

    Wrote single port version of port scanner

--- ch2-tcp-scanners-proxies/port_scanner.go
+++ ch2-tcp-scanners-proxies/port_scanner.go
@@ -6,8 +6,12 @@ import (
 )
 
 func main() {
+	// Dial takes two args: (network, address string)
+	// network specifies the kind of connection: tcp, udp, unix, etc.
+	// address specifies the host to which you want to connect.
+	// address will be of the format "host:port"
 	_, err := net.Dial("tcp", "canme.nmap.org:80")
-	if err != nil {
+	if err == nil {
 		fmt.Println("Connection successful")
 	}
 }
