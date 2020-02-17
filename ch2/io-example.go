ae302cdfde8c449c243c977bda8b105f18738d33
Julius Zerwick <jrzerwick@gmail.com>
2020-02-17 10:44:17 -0500

    Renamed directory and started tcp-proxy exercises

---
+++ ch2/io-example.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+// FooReader defines an io.Reader to read from stdin.
+type FooReader struct{}
+
+// Read reads data from stdin.
+func (fooReader *FooReader) Read(b []byte) (int, error) {
+	fmt.Print("in > ")
+	return os.Stdin.Read(b)
+}
+
+// FooWriter defines an io.Writer to write to stdout.
+type FooWriter struct{}
+
+// Write writes data to stdout.
+func (fooWriter *FooWriter) Write(b []byte) (int, error) {
+	fmt.Print("out > ")
+	return os.Stdout.Write(b)
+}
+
+func main() {
+	// Instantiate reader and writer.
+	var (
+		reader FooReader
+		writer FooWriter
+	)
+
+	// Create buffer to hold input/output.
+	input := make([]byte, 4096)
+
+	// Use reader to read input.
+	s, err := reader.Read(input)
+	if err != nil {
+		log.Fatalln("unable to read data")
+	}
+	fmt.Printf("Read %d bytes from stdin\n", s)
+
+	// Use writer to write output.
+	s, err = writer.Write(input)
+	if err != nil {
+		log.Fatalln("Unable to write data")
+	}
+	fmt.Printf("Wrote %d bytes to stdout\n", s)
+}
